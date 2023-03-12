be3f338885fe2992e98e8fea1301ac903db5d07d
dylan <Dylanomahonyirish@gmail.com>
2023-03-12 21:16:14 +0000

    Added main function

---
+++ Main.C
@@ -0,0 +1,7 @@
+#include <stdio.h> // input-output library
+#include <stdlib.h> // standard library
+extern
+int main( int argc, char **argv ) {
+printf( "Main function.\n" );
+return EXIT_SUCCESS;
+}
