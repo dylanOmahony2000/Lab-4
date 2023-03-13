ba489b241e8d597b455feadc9f444e28a98f76b8
dylan <Dylanomahonyirish@gmail.com>
2023-03-13 21:36:19 +0000

    added binary search tree functions

--- BST.C
+++ BST.C
@@ -1,16 +1,63 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <cstdlib>
 
 // Node struct
-
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
 
 // create Node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
 
 
 // insert function
+struct Node* insert(struct Node* root, int data) {
+    if (root == NULL) {
+        return createNode(data);
+    }
+    if (data < root->data) {
+        root->left = insert(root->left, data);
+    } else if (data > root->data) {
+        root->right = insert(root->right, data);
+    }
+    return root;
+}
 
 
 // find function
-
+struct Node* find(struct Node* root, int data) {
+    if (root == NULL || root->data == data) {
+        return root;
+    }
+    if (data < root->data) {
+        return find(root->left, data);
+    } else {
+        return find(root->right, data);
+    }
+}
+// print tree
+void printTree(struct Node* root) {
+    if (root != NULL) {
+        printTree(root->left);
+        printf("%d ", root->data);
+        printTree(root->right);
+    }
+}
 
 // create binary search tree
+struct Node* createBST(int* arr, int n) {
+    struct Node* root = NULL;
+    for (int i = 0; i < n; i++) {
+        root = insert(root, arr[i]);
+    }
+    return root;
+}
--- Main.C
+++ Main.C
@@ -1,7 +1,21 @@
 #include <stdio.h> // input-output library
-#include <stdlib.h> // standard library
-extern
-int main( int argc, char **argv ) {
-printf( "Main function.\n" );
-return EXIT_SUCCESS;
+
+#include "BST.C"
+
+int main() {
+    int arr[] = {5, 2, 8, 1, 3, 6, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    struct Node* root = createBST(arr, n);
+    printf("Binary Search Tree: ");
+    printTree(root);
+    printf("\n");
+    int searchValue = 6;
+    struct Node* searchResult = find(root, searchValue);
+    if (searchResult != NULL) {
+        printf("Found value %d in tree!\n", searchValue);
+    } else {
+        printf("Value %d not found in tree.\n", searchValue);
+    }
+    
+    return 0;
 }
