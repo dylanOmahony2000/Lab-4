72e9b63ae54b86e3b24922aebc5d2467365942ae
dylan <Dylanomahonyirish@gmail.com>
2023-03-14 19:10:53 +0000

    binary tree is working need to fix pointers

--- Main.C
+++ Main.C
@@ -1,21 +1,35 @@
 #include <stdio.h> // input-output library
-
+#include <stdlib.h> // standard library
 #include "BST.C"
+#include <time.h>
 
 int main() {
-    int arr[] = {5, 2, 8, 1, 3, 6, 9};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    struct Node* root = createBST(arr, n);
-    printf("Binary Search Tree: ");
-    printTree(root);
-    printf("\n");
-    int searchValue = 6;
-    struct Node* searchResult = find(root, searchValue);
-    if (searchResult != NULL) {
-        printf("Found value %d in tree!\n", searchValue);
-    } else {
-        printf("Value %d not found in tree.\n", searchValue);
+    
+    // create the binary search tree
+    struct Node* root = NULL;
+    
+    
+    // seed the random number generator
+    srand(time(NULL));
+    
+    // insert 10 random numbers between 1 and 20 into the tree
+    for (int i = 0; i < 10; i++) {
+        int num = rand() % 20 + 1;
+        root = insert(root, num);
     }
+    // search for an item in the tree
+    int item = 3;
+    struct Node* found = find(root, item);
+    if (found != NULL) {
+        printf("Found item %d in the tree!\n", item);
+    } else {
+        printf("Item %d not found in the tree.\n", item);
+    }
+    
+    // print the structure of the tree
+    printf("Tree structure:\n");
+    printTree(root, 0);
+    
     
     return 0;
 }
